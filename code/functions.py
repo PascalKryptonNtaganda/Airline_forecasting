7719ced1ba3a8fb708bf939214a520448b0e4d3a
Pascal Ntaganda <95452085+PascalKryptonNtaganda@users.noreply.github.com>
2023-07-31 21:26:26 +0000

    Function to forecast using multiplicative model

--- code/functions.py
+++ code/functions.py
@@ -29,3 +29,35 @@ val.append(row['cum_bookings']/total_booking)
  booking_pace[key] = float(sum(value))/len(value)
  return booking_pace
 
+def mult_model(df, booking_pace):
+ #for each departure date finds a forecast
+ #using mulitplicative model
+ df = validation_df.dropna(how='any',axis=0)
+ df['multiplicative'] = 0
+ fmt = '%Y-%m-%d'
+ df['departure_date'] = df['departure_date'].astype('datetime64[ns]')
+ df['booking_date'] = df['booking_date'].astype('datetime64[ns]')
+
+ for index, row in df.iterrows():
+ days_prior = row['departure_date'] - row['booking_date']
+ days_prior = days_prior.days
+ if math.isclose(booking_pace[days_prior], 0, rel_tol=1e-2):
+ df.loc[index, 'multiplicative'] = 0
+ else:
+ 
+ #formula from slides
+ df.loc[index, 'multiplicative'] =
+row['cum_bookings']/booking_pace[days_prior]
+ for index, row in df.iterrows():
+ 
+ #finds the absolute error for our forecast to naive forecast and shows sum
+difference
+ df.loc[index, 'Our Forecast Absolute Error'] = abs(row['multiplicative'] -
+row['cum_bookings'])
+ df.loc[index, 'Naive Forecast Absolute Error'] = abs(row['naive_fcst'] -
+row['cum_bookings'])
+ MASE = df['Our Forecast Absolute Error'].sum() / df['Naive Forecast Absolute
+Error'].sum()
+ print("Our model reduced errors by " + str(round(MASE,2)) + " percent or by " +
+str(round(MASE/100*9154)) + " tickets.")
+ return df
