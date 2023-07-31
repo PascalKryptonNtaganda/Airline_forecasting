c01d030fcf5861295992672e5abea0e42fdacdd0
Pascal Ntaganda <95452085+PascalKryptonNtaganda@users.noreply.github.com>
2023-07-31 21:25:44 +0000

    function to get booking pace

--- code/functions.py
+++ code/functions.py
@@ -1,1 +1,31 @@
 
+def get_booking_pace(df):
+ fmt = '%Y-%m-%d'
+ #booking pace for days prior
+ #key: days prior
+ #value: rate. percentage of final/total booking already booked before days prior
+ booking_pace = {}
+ final_booking = df.loc[df['booking_date'] == df['departure_date']]
+
+ #loop finds all the on-the-book bookings for any days prior
+ for index, row in df.iterrows():
+ departure = row['departure_date']
+ booking = row['booking_date']
+ total_booking = final_booking.loc[final_booking['departure_date'] ==
+departure]
+ total_booking = total_booking['cum_bookings']
+ days_prior = departure - booking
+ days_prior = days_prior.days
+
+ if days_prior in booking_pace and booking_pace[days_prior]:
+ val = booking_pace[days_prior]
+ booking_pace[days_prior] =
+val.append(row['cum_bookings']/total_booking)
+ else:
+ booking_pace[days_prior] = [row['cum_bookings']/total_booking]
+
+ #finds average booking pace for any days prior
+ for key, value in booking_pace.items():
+ booking_pace[key] = float(sum(value))/len(value)
+ return booking_pace
+
